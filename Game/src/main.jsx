5dbdf9a30ab2825702a71bb91f3d4e006d3d9844
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-16 18:01:51 +0300

    Routing defined

--- Game/src/main.jsx
+++ Game/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import App from './App.jsx'
+import  How  from './pages/How.jsx' 
+import './index.css'
+import "@fontsource/fredoka"
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/howto" element={<How />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
