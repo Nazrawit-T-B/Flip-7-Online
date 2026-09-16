4824d858d8a54e625a407e4c6653984f6e9a9f53
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-16 18:02:17 +0300

    Landing page to select the number of players

--- Game/src/App.jsx
+++ Game/src/App.jsx
@@ -1,6 +1,54 @@
-function App(){
-  return(
-    <h1>This is a trial</h1>
-  )
-}
+import { useState } from "react";
+import "./app.css";
+import {useNavigate} from 'react-router-dom';
+const App = () => {
+  const nav=useNavigate();
+  const [players, setplayers] = useState(2);
+
+  const addPlayer = () => {
+    const value = players + 1;
+    if (value < 6) {
+      setplayers(value);
+    } else {
+      setplayers(6);
+    }
+  };
+
+  const removePlayer = () => {
+    const value = players - 1;
+    if (value > 2) {
+      setplayers(value);
+    } else {
+      setplayers(2);
+    }
+  };
+
+  const navigate=()=>{
+    nav('./pages/howto')
+  }
+
+  return (
+
+    <div className="flex flex-col items-center justify-center gap-10 m-30">
+      <h1>FLIP 7</h1>
+      <h3> PUSH YOUR LUCK</h3>
+      <div className="flex flex-row items-center justify-center gap-10">
+        <div>
+          <button onClick={addPlayer}>Add player +</button>
+        </div>
+        <div className="player-count">
+          <h1>{players}</h1>
+        </div>
+        <div>
+          <button onClick={removePlayer}>Remove player -</button>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-10">
+        <button>Play Game </button>
+        <button onClick={()=>{nav('/howto')}}>How To Play</button>
+      </div>
+    </div>
+
+  );
+};
 export default App;
