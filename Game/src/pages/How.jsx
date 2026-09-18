fb39998735f884458bd3a1ca42b6f8b4f599c1ae
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-18 22:02:03 +0300

    How to play page final construction

--- Game/src/pages/How.jsx
+++ Game/src/pages/How.jsx
@@ -1,8 +1,18 @@
 import {useNavigate} from 'react-router-dom';
+import RuleCTA from '/src/components/RuleCTA';
+import Objective from '../components/ObjectiveCTA';
+import Anatomy from '../components/AnatomyCTA';
+import Special from '../components/SpecialCTA';
+import ProTip from '../components/ProtipCTA';
 const How=()=>{
     return (
 
         <div>
+            <RuleCTA/>
+            <Objective/>
+            <Anatomy/>
+            <Special/>
+            <ProTip/>
             <p>This is a trial</p>
         </div>
     )
