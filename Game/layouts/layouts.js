5ce14b683348039d6a4c24f96340c9807bea0a01
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-22 18:31:28 +0300

    Layout logic for the cards

---
+++ Game/layouts/layouts.js
@@ -0,0 +1,138 @@
+const LAYOUTS = {
+  2: [
+    {
+      name: "Player 1",
+      position: "bottom",
+      x: 400,
+      y: 100,
+    },
+    {
+      name: "Player 2",
+      position: "top",
+      x: 400,
+      y: 650,
+    },
+  ],
+
+  3: [
+    {
+      name: "Player 1",
+      position: "bottom",
+      x: 100,
+      y: 400,
+    },
+    {
+      name: "Player 2",
+      position: "left",
+      x: 700,
+      y: 400,
+    },
+    {
+      name: "Player 3",
+      position: "right",
+      x: 400,
+      y: 650,
+    },
+  ],
+
+  4: [
+    {
+      name: "Player 1",
+      position: "bottom",
+      x: 400,
+      y: 100,
+    },
+    {
+      name: "Player 2",
+      position: "left",
+      x: 700,
+      y: 400,
+    },
+    {
+      name: "Player 3",
+      position: "top",
+      x: 400,
+      y: 650,
+    },
+    {
+      name: "Player 4",
+      position: "right",
+      x: 100,
+      y: 400,
+    },
+  ],
+
+  5: [
+    {
+      name: "Player 1",
+      position: "bottom",
+      x: 150,
+      y: 150,
+    },
+    {
+      name: "Player 2",
+      position: "bottom-left",
+      x: 650,
+      y: 150,
+    },
+    {
+      name: "Player 3",
+      position: "top-left",
+      x: 650,
+      y: 600,
+    },
+    {
+      name: "Player 4",
+      position: "top-right",
+      x: 400,
+      y: 650,
+    },
+    {
+      name: "Player 5",
+      position: "bottom-right",
+      x: 150,
+      y: 600,
+    },
+  ],
+
+  6: [
+    {
+      name: "Player 1",
+      position: "bottom",
+      x: 150,
+      y: 150,
+    },
+    {
+      name: "Player 2",
+      position: "bottom-left",
+      x: 400,
+      y: 100,
+    },
+    {
+      name: "Player 3",
+      position: "top-left",
+      x: 650,
+      y: 150,
+    },
+    {
+      name: "Player 4",
+      position: "top",
+      x: 650,
+      y: 600,
+    },
+    {
+      name: "Player 5",
+      position: "top-right",
+      x: 400,
+      y: 650,
+    },
+    {
+      name: "Player 6",
+      position: "bottom-right",
+      x: 150,
+      y: 600,
+    },
+  ],
+};
+
+export default LAYOUTS;
