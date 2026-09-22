5053700b92d0fa68a2395b679a441184313c7e3c
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-22 18:32:25 +0300

    Route added to game

--- Game/src/main.jsx
+++ Game/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import App from './App.jsx'
 import  How  from './pages/How.jsx' 
+import Game from './pages/game.jsx'
 import './index.css'
 import "@fontsource/fredoka"
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/howto" element={<How />} />
+        <Route path="/play" element={<Game/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
